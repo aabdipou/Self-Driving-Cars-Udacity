90fbc3a5e919cd19528beb7f216b55c4e38108b6
Arshia <Arshiaabdipour@gmail.com>
2021-06-03 21:27:40 -0400

    first commit - lessons 1 & 2 of computer vision

---
+++ Computer Vision/02 Reigon of Interest/test.py
@@ -0,0 +1,53 @@
+import tkinter
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+
+# Read in the image and print out some stats
+image = mpimg.imread('C://Users//Arshia//Desktop//road.jpg')
+print('This image is: ',type(image), 
+         'with dimensions:', image.shape)
+
+# Grab the x and y size and make a copy of the image
+ysize = image.shape[0]
+xsize = image.shape[1]
+
+color_select = np.copy(image)
+
+# Define our color selection criteria
+# Note: if you run this code, you'll find these are not sensible values!!
+# But you'll get a chance to play with them soon in a quiz
+red_threshold = 205
+green_threshold = 205
+blue_threshold = 205
+rgb_threshold = [red_threshold, green_threshold, blue_threshold]
+
+thresholds = (image[:,:,0] < rgb_threshold[0]) | (image[:,:,1] < rgb_threshold[1])  | (image[:,:,2] < rgb_threshold[2])
+color_select[thresholds] = [0,0,0]
+
+region_select = np.copy(image)
+# Define a triangle region of interest 
+# Keep in mind the origin (x=0, y=0) is in the upper left in image processing
+# Note: if you run this code, you'll find these are not sensible values!!
+# But you'll get a chance to play with them soon in a quiz 
+left_bottom = [0, 539]
+right_bottom = [900, 300]
+apex = [400, 0]
+
+# Fit lines (y=Ax+B) to identify the  3 sided region of interest
+# np.polyfit() returns the coefficients [A, B] of the fit
+fit_left = np.polyfit((left_bottom[0], apex[0]), (left_bottom[1], apex[1]), 1)
+fit_right = np.polyfit((right_bottom[0], apex[0]), (right_bottom[1], apex[1]), 1)
+fit_bottom = np.polyfit((left_bottom[0], right_bottom[0]), (left_bottom[1], right_bottom[1]), 1)
+
+# Find the region inside the lines
+XX, YY = np.meshgrid(np.arange(0, xsize), np.arange(0, ysize))
+region_thresholds = (YY > (XX*fit_left[0] + fit_left[1])) & (YY > (XX*fit_right[0] + fit_right[1])) & (YY < (XX*fit_bottom[0] + fit_bottom[1]))
+                    
+
+print(temp > temp2)
+# # Display the image                 
+# plt.imshow(color_select)
+# plt.show()
